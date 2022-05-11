769c42a9eb7fdda7ae0a47ed91494534987c1212
alessiomillauro <alessio.millauro@gmail.com>
2022-05-11 13:37:21 +0200

    add DI support in MainActivity class

--- app/src/main/java/com/android/gamerenter/MainActivity.kt
+++ app/src/main/java/com/android/gamerenter/MainActivity.kt
@@ -2,7 +2,10 @@ package com.android.gamerenter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
