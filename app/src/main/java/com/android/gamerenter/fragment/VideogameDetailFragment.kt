d49c4c4876ae73cbaf06899c848619c38517d0f5
alessiomillauro <alessio.millauro@gmail.com>
2022-06-09 19:02:26 +0200

    create videogame detail fragment

--- app/src/main/java/com/android/gamerenter/fragment/VideogameDetailFragment.kt
+++ app/src/main/java/com/android/gamerenter/fragment/VideogameDetailFragment.kt
@@ -3,22 +3,22 @@ package com.android.gamerenter.fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.Menu.NONE
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import androidx.constraintlayout.helper.widget.Flow
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL
 import androidx.recyclerview.widget.RecyclerView
-import com.android.gamerenter.FIREBASE_STORAGE_GAMES_ROOT
-import com.android.gamerenter.FIREBASE_STORAGE_LOCATION
 import com.android.gamerenter.R
 import com.android.gamerenter.adapter.VideogameImagesAdapter
 import com.android.gamerenter.dialog.DIALOG_TYPE
 import com.android.gamerenter.dialog.GeneralDialog
+import com.android.gamerenter.model.PlatformModel
 import com.android.gamerenter.viewmodel.VideogameDetailViewModel
 import com.bumptech.glide.Glide
 import com.google.android.material.card.MaterialCardView
@@ -27,8 +27,6 @@ import com.google.firebase.storage.FirebaseStorage
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.StringBuilder
 import java.text.SimpleDateFormat
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import java.util.*
 import javax.inject.Inject
 
@@ -42,6 +40,7 @@ class VideogameDetailFragment : Fragment() {
     @Inject
     lateinit var firebaseStorage: FirebaseStorage
 
+    private lateinit var parent: ConstraintLayout
     private lateinit var image: ImageView
     private lateinit var title: TextView
     private lateinit var overview: MaterialTextView
@@ -49,12 +48,13 @@ class VideogameDetailFragment : Fragment() {
     private lateinit var release: MaterialTextView
     private lateinit var playtime: MaterialTextView
     private lateinit var metascoreValue: MaterialTextView
-    private lateinit var containerPlatforms: LinearLayout
+    private lateinit var containerPlatforms: Flow
     private lateinit var rvImages: RecyclerView
     private lateinit var btnRent: Button
     private lateinit var btnPeriodRent: Button
 
     private var periodRent = -1
+    private var platformList = listOf<PlatformModel>()
 
     @Inject
     lateinit var imagesAdapter: VideogameImagesAdapter
@@ -73,6 +73,7 @@ class VideogameDetailFragment : Fragment() {
         val item = args.videogameModel
 
         val container: MaterialCardView = view.findViewById(R.id.container_overview)
+        parent = view.findViewById(R.id.container)
         title = view.findViewById(R.id.title)
         image = view.findViewById(R.id.toolbar_image)
         overview = container.findViewById(R.id.overview)
@@ -84,6 +85,39 @@ class VideogameDetailFragment : Fragment() {
         rvImages = view.findViewById(R.id.rv_images)
         btnRent = view.findViewById(R.id.rent)
         btnPeriodRent = view.findViewById(R.id.rent_period)
+
+        videogameDetailViewModel.platformListLiveData.observe(viewLifecycleOwner) {
+            platformList = it
+
+            for (platform in item.platforms) {
+                for (plat in platformList) {
+                    if (plat.id == platform) {
+                        val view = layoutInflater.inflate(R.layout.item_videogame_platform, null)
+                        view.id = plat.id
+                        val imgPlatform = view.findViewById<ImageView>(R.id.image_platform)
+                        val namePlatform = view.findViewById<TextView>(R.id.name_platform)
+
+                        namePlatform.text = plat.name
+                        if (!plat.image.isNullOrBlank()) {
+                            val ref = firebaseStorage.getReferenceFromUrl(plat.image.toString())
+                            ref.downloadUrl.addOnCompleteListener {
+                                context?.let { it1 ->
+                                    Glide.with(it1).load(it.result).into(imgPlatform)
+                                }
+                            }
+                        } else {
+                            context?.let { it1 ->
+                                Glide.with(it1)
+                                    .load(resources.getDrawable(R.drawable.ic_placeholder_console))
+                                    .into(imgPlatform)
+                            }
+                        }
+                        parent.addView(view)
+                        containerPlatforms.addView(view)
+                    }
+                }
+            }
+        }
 
         firebaseStorage.getReferenceFromUrl(item.background_image)
             .downloadUrl
@@ -103,34 +137,40 @@ class VideogameDetailFragment : Fragment() {
         genre.text = getGenresList(item.genre)
         playtime.text =
             String.format(resources.getString(R.string.average_playtime), item.playtime.toString())
-        //metascoreValue.text = item.
+        metascoreValue.text = item.metascore.toString()
 
+        imagesAdapter.submitList(item.images)
         rvImages.apply {
             layoutManager = LinearLayoutManager(context, HORIZONTAL, false)
             adapter = imagesAdapter
         }
 
-        btnPeriodRent.setOnClickListener {
-            val popupMenu = PopupMenu(context, it)
-            for (i in 1..7) {
-                popupMenu.menu.add(
-                    NONE,
-                    i,
-                    NONE,
-                    if (i > 1) "$i GIORNI" else "$i GIORNO"
-                )
-            }
-            popupMenu.show()
+        btnPeriodRent.apply {
+            text = "1 GIORNO"
+            isEnabled = item.rent_max_period != 1
+            setOnClickListener {
 
-            popupMenu.setOnMenuItemClickListener(object : PopupMenu.OnMenuItemClickListener {
-                override fun onMenuItemClick(item: MenuItem?): Boolean {
-                    periodRent = item?.itemId!!
-                    return true
+                val popupMenu = PopupMenu(context, it)
+                for (i in 1..item.rent_max_period) {
+                    popupMenu.menu.add(
+                        NONE,
+                        i,
+                        NONE,
+                        if (i > 1) "$i GIORNI" else "$i GIORNO"
+                    )
                 }
-            })
+                popupMenu.show()
+
+                popupMenu.setOnMenuItemClickListener { item ->
+                    periodRent = item?.itemId!!
+                    text = if (periodRent > 1) "$periodRent GIORNI" else "$periodRent GIORNO"
+                    true
+                }
+            }
         }
 
         btnRent.apply {
+            text = context.getString(R.string.rent_action_title)
             isEnabled = item.rent_copy_available < item.rent_copy_total
             setOnClickListener {
                 videogameDetailViewModel.rent(item.id, periodRent, System.currentTimeMillis())
@@ -138,8 +178,13 @@ class VideogameDetailFragment : Fragment() {
         }
 
         videogameDetailViewModel.updateRentResutl.observe(
-            viewLifecycleOwner,
-            androidx.lifecycle.Observer { t ->  })
+            viewLifecycleOwner
+        ) { t ->
+            GeneralDialog(
+                if (t) DIALOG_TYPE.SUCCESS else DIALOG_TYPE.ERROR,
+                if (t) "Dati aggiornati correttamente" else "Si è verificato un errore. Riprovarez"
+            ).show(parentFragmentManager, "dialog")
+        }
     }
 
     private fun getGenresList(genres: List<String>): String {
--- app/src/main/java/com/android/gamerenter/viewmodel/VideogameDetailViewModel.kt
+++ app/src/main/java/com/android/gamerenter/viewmodel/VideogameDetailViewModel.kt
@@ -5,21 +5,26 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import com.android.gamerenter.repository.PlatformRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class VideogameDetailViewModel @Inject constructor(
     private val platformRepository: PlatformRepository
 ) : ViewModel() {
 
     var updateRentResutl: MutableLiveData<Boolean> = MutableLiveData()
 
-
     fun rent(id: Int, periodRent: Int, timestamp: Long) {
         viewModelScope.launch(Dispatchers.IO) {
             val result = platformRepository.updateRentOperation(id, periodRent, timestamp)
             updateRentResutl.postValue(result)
         }
+    }
+
+    val platformListLiveData = liveData(Dispatchers.IO) {
+        emit(platformRepository.getPlatformsList())
     }
 }
