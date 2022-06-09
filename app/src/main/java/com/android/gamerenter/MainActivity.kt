e22e7aa91ab2342c53d1e80490afc396789983ff
alessiomillauro <alessio.millauro@gmail.com>
2022-06-09 19:01:34 +0200

    manage click on bottom menu

--- app/src/main/java/com/android/gamerenter/MainActivity.kt
+++ app/src/main/java/com/android/gamerenter/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.android.gamerenter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.firebase.FirebaseApp
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val menu = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        menu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> findNavController(R.id.nav_host_fragment).navigate(R.id.dashboardFragment)
+                R.id.search -> findNavController(R.id.nav_host_fragment).navigate(R.id.searchVideogameFragment)
+                R.id.rent -> findNavController(R.id.nav_host_fragment).navigate(R.id.rentedVideogameFragment)
+            }
+            return@setOnItemSelectedListener true
+        }
     }
+
+
 }
